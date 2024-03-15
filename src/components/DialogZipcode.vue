23f2c8068d6345383404ced118a614fb2d3bf222
DESKTOP-33T0PAA\hakus <hakusa@naver.com>
2024-03-15 17:53:36 +0900

    20240315

--- src/components/DialogZipcode.vue
+++ src/components/DialogZipcode.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <v-dialog v-model="dialog" scrollable width="450">
+    <v-dialog v-model="dialog" scrollable style="height: 800;width: 1000;">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn outlined color="black" width="80" v-bind="attrs" v-on="on">
+        <v-btn outlined color="black" width="80" v-bind="attrs" v-on="on" @click="open">
           주소찾기
         </v-btn>
       </template>
-      <v-card class="card-dialog">
-        <div class="dialog-header">
+      <v-card class="card-dialog" >
+        <div class="dialog-header" >
           <v-btn icon color="black" class="dialog-close" @click="close">
             <v-icon>mdi-close</v-icon>
           </v-btn>
           <v-card-title class="dialog-title"> 우편번호 찾기 </v-card-title>
         </div>
-        <div class="dialog-body">
-          <div class="zipcode-group">우편번호 찾기 불러오는 곳</div>
+        <div class="dialog-body" v-on="on"  ref="embed" >
+          <div class="zipcode-group"   ></div>
         </div>
       </v-card>
     </v-dialog>
@@ -30,6 +30,62 @@ export default {
   data: () => ({
     dialog: false,
   }),
+
+  methods: {
+    init(){
+      this.$emit("move", "a", "b");
+    },
+    close(){
+      this.dialog = false;
+    },
+    open(){
+      this.dialog = true;
+    },
+    created() {
+      this.showAddr();
+    },
+
+    showAddr() {
+        // let _width = '650';
+        // let _height = '380';
+        
+        // let _left = Math.ceil(( window.screen.width - _width )/2);
+        // let _top = Math.ceil(( window.screen.height - _height )/2);
+        new window.daum.Postcode({
+          oncomplete: (data) => {
+              // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+  
+              // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+              // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+              let fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+              let extraRoadAddr = ''; // 도로명 조합형 주소 변수
+
+              // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+              // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+              if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                  extraRoadAddr += data.bname;
+              }
+              // 건물명이 있고, 공동주택일 경우 추가한다.
+              if(data.buildingName !== '' && data.apartment === 'Y'){
+                extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+              }
+              // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+              if(extraRoadAddr !== ''){
+                  extraRoadAddr = ' (' + extraRoadAddr + ')';
+              }
+              // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+              if(fullRoadAddr !== ''){
+                  fullRoadAddr += extraRoadAddr;
+              }
+  
+              // 우편번호와 주소 정보를 해당 필드에 넣는다.
+              this.zip = data.zonecode; //5자리 새우편번호 사용
+              this.addr1 = fullRoadAddr;
+          }
+        }).embed(this.$refs.embed)
+
+      },
+  },
 };
 </script>
 
--- src/views/energy/ApplySocialWelfare1.vue
+++ src/views/energy/ApplySocialWelfare1.vue
@@ -253,9 +253,7 @@
                           <v-col class="pb-0">
                             <v-row no-gutters>
                               <v-col cols="auto">
-                                <v-btn outlined color="black" width="80" v-bind="attrs" @click="showAddr">
-                                  주소찾기
-                                </v-btn>
+                                <DialogZipcode @move="inputAddr"/>
                               </v-col>
                               <v-col cols="3">
                                 <v-text-field
@@ -344,6 +342,7 @@
 // @ is an alias to /src
 import Title from "@/components/Title.vue";
 import Terms from "@/components/Terms.vue";
+import DialogZipcode from "@/components/DialogZipcode.vue"
 import Privacy from "@/components/Privacy.vue";
 
 
@@ -353,7 +352,7 @@ export default {
     Title,
     Terms,
     Privacy,
-    
+    DialogZipcode,
   },
   data: () => ({
     breadcrumbs: [
@@ -501,7 +500,7 @@ export default {
       // }
 
       //alert(this.$refs.pform.validate());
-
+      alert(DialogZipcode.dialog);
       if(this.validate()){
         
         alert('test');
@@ -512,40 +511,13 @@ export default {
       //this.$router.push('/applysocialwelfare5');
     },
 
-    showAddr() {
-        new window.daum.Postcode({
-          oncomplete: (data) => {
-              // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-  
-              // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
-              // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-              let fullRoadAddr = data.roadAddress; // 도로명 주소 변수
-              let extraRoadAddr = ''; // 도로명 조합형 주소 변수
-  
-              // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-              // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-              if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                  extraRoadAddr += data.bname;
-              }
-              // 건물명이 있고, 공동주택일 경우 추가한다.
-              if(data.buildingName !== '' && data.apartment === 'Y'){
-                extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-              }
-              // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-              if(extraRoadAddr !== ''){
-                  extraRoadAddr = ' (' + extraRoadAddr + ')';
-              }
-              // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
-              if(fullRoadAddr !== ''){
-                  fullRoadAddr += extraRoadAddr;
-              }
-  
-              // 우편번호와 주소 정보를 해당 필드에 넣는다.
-              this.zip = data.zonecode; //5자리 새우편번호 사용
-              this.addr1 = fullRoadAddr;
-          }
-        }).open();
-      },
+    inputAddr(zip, addr1){
+      this.zipcode = zip;
+      this.addr1 = addr1;
+      //this.addr2 = addr2;
+    },
+
+    
   },
 };
 </script>
